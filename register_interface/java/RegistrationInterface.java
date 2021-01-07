cdf6595c83fd7e1df83c0ff5c1b7c6d62e5e987d
sudeshiuthpala <57354717+sudeshiuthpala@users.noreply.github.com>
2021-01-08 00:06:09 +0530

    Main Registration UI

---
+++ register_interface/java/RegistrationInterface.java
@@ -0,0 +1,14 @@
+package com.example.smartvan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrationInterface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_interface);
+    }
+}
