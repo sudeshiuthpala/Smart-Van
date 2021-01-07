e67b863898e38872fe319ec44407f4e63a08cc14
sudeshiuthpala <57354717+sudeshiuthpala@users.noreply.github.com>
2021-01-08 00:13:28 +0530

    Forget Password UI

---
+++ Forget Password/java/ForgetPassword3.java
@@ -0,0 +1,14 @@
+package com.example.smartvan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgetPassword3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password3);
+    }
+}
