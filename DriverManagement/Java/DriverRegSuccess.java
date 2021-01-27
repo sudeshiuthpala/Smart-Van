e9394c78d69526bb6f60dac92a1436737ec1d20b
sudeshiuthpala <57354717+sudeshiuthpala@users.noreply.github.com>
2021-01-27 23:19:22 +0530

    +Driver Reg Success interface

---
+++ DriverManagement/Java/DriverRegSuccess.java
@@ -0,0 +1,14 @@
+package com.example.smartvan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DriverRegSuccess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_reg_success);
+    }
+}
