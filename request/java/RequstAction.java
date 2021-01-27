31f6499db2ee87c18c6929119b2f21a0f0603ff5
sudeshiuthpala <57354717+sudeshiuthpala@users.noreply.github.com>
2021-01-27 20:04:59 +0530

    only interface

---
+++ request/java/RequstAction.java
@@ -0,0 +1,14 @@
+package com.example.smartvan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RequstAction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_requst_action);
+    }
+}
