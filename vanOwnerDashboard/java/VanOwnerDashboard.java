91b8bddad0a7d97ae69b4b40bcef596262cdbd6d
sudeshiuthpala <57354717+sudeshiuthpala@users.noreply.github.com>
2021-01-27 20:00:02 +0530

    only interface

---
+++ vanOwnerDashboard/java/VanOwnerDashboard.java
@@ -0,0 +1,14 @@
+package com.example.smartvan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VanOwnerDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_van_owner_dashboard);
+    }
+}
